54123a6f9d6d0464c3f4d07893d4254a0e3725e3
ambodalle <ambodalle.st@gmail.com>
2017-07-28 16:15:17 +0700

    add action

--- redux-pasword-manager/src/actions/index.js
+++ redux-pasword-manager/src/actions/index.js
@@ -12,3 +12,58 @@ export const SET_ID = 'SET_ID'
 export const RESET = ' RESET'
 export const SET_INDEX = 'SET_INDEX'
 export const SET_CREATED = 'SET_CREATED'
+
+export function setCreated(value) {
+     return {
+          type: SET_CREATED,
+          value
+     }
+}
+
+export function setIndex(value) {
+     return {
+          type: SET_INDEX,
+          value
+     }
+}
+
+export function reset() {
+     return {
+          type: RESET
+     }
+}
+
+export function setId(value) {
+     return {
+          type: SET_ID,
+          value
+     }
+}
+
+export function setPassword(value) {
+     return {
+          type: SET_PASSWORD,
+          value
+     }
+}
+
+export function setUsername (value) {
+     return {
+          type: SET_USERNAME,
+          value
+     }
+}
+
+export function setUrl (value) {
+     return {
+          type: SET_URL,
+          value
+     }
+}
+
+export function seedPasswordSuccess(value) {
+     return {
+          type: SEED_PASSWORD,
+          value
+     }
+}
